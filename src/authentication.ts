ab51cfd40589a2d8e199ca7a082f59063501156a
Murilo Ferreira <rillofelix@hotmail.com>
2024-07-30 16:23:27 -0300

    Exemplos de schemas e documentação

--- src/authentication.ts
+++ src/authentication.ts
@@ -4,6 +4,7 @@ import { LocalStrategy } from '@feathersjs/authentication-local';
 import { expressOauth } from '@feathersjs/authentication-oauth';
 
 import { Application } from './declarations';
+import { ServiceSwaggerOptions } from 'feathers-swagger';
 
 declare module './declarations' {
   interface ServiceTypes {
@@ -11,11 +12,37 @@ declare module './declarations' {
   }
 }
 
-export default function(app: Application): void {
+export default function (app: Application): void {
   const authentication = new AuthenticationService(app);
 
   authentication.register('jwt', new JWTStrategy());
   authentication.register('local', new LocalStrategy());
+
+  const docs: ServiceSwaggerOptions = {
+    description: 'Serviço de autenticação de usuários',
+    definitions: {
+      usuarios: {
+        "type": "object",
+        "required": [
+          "text"
+        ],
+        "properties": {
+          "email": {
+            "type": "string",
+            "description": "O email do Usuário",
+            "example": "murilo@email.com"
+          },
+          "senha": {
+            "type": "string",
+            "description": "A senha do Usuário",
+            "example": "senha1234"
+          }
+        }
+      }
+    }
+  };
+
+
 
   app.use('/authentication', authentication);
   app.configure(expressOauth());
--- src/services/usuarios/usuarios.class.ts
+++ src/services/usuarios/usuarios.class.ts
@@ -4,23 +4,45 @@ import { ServiceSwaggerOptions } from 'feathers-swagger';
 
 export class Usuarios extends Service {
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
-  docs:ServiceSwaggerOptions={
-    description: 'A service to send and receive messages',
-  definitions: {
-    usuarios: {
-      "type": "object",
-      "required": [
-        "text"
-      ],
-      "properties": {
-        "id": {
-          "type": "number",
-          "description": "o id do Usuario"
-        }
-      }
-    }
-  }}
   constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
   }
+  docs:ServiceSwaggerOptions={
+    description: 'Serviço CRUD de usuários',
+    definitions: {
+      usuarios: {
+        "type": "object",
+        "required": [
+          "text"
+        ],
+        "properties": {
+          "id": {
+            "type": "number",
+            "description": "O id do usuário",
+            "example": "0"
+          },
+          "nome":{
+            "type": "string",
+            "description" : "O nome do Usuário",
+            "example": "Murilo"
+          },
+          "email":{
+            "type": "string",
+            "description" : "O email do Usuário",
+            "example": "murilo@email.com"
+          },
+          "senha":{
+            "type": "string",
+            "description" : "A senha do Usuário",
+            "example": "senha1234"
+          },
+          "CPF":{
+            "type": "integer",
+            "description": "O CPF do usuário",
+            "example": "12345678910"
+          },
+        }
+      }
+    }
+  };
 }
